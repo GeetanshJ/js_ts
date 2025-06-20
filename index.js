e8ad8ec6812eb92f5fcfe33319b78002e2a1f570
Geetansh Jain <jaingeetansh@gmail.com>
2025-06-20 11:30:07 +0530

    .

--- index.js
+++ index.js
@@ -1,118 +1,20 @@
 
-// Object.create
-// let proto = {
-//     name:"geetansh",
-//     age:22,
-//     greet: function(){
-//             console.log(`Hi ${this.name}`);
-            
-//         },
 
-//         getAge : () => {
-//             console.log(`You are ${this.age} years old`);
-//         }
-// };
+let stream;
 
-// let obj = Object.create(proto);
-// obj.greet();
-// obj.getAge(proto); // undefined beacuse lexical this nahi mil raha
+async function play() {
+  stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "user" },
+    audio: true,
+  });
+  document.querySelector("video").srcObject = stream;
+}
 
+async function pause() {
+  stream.getTracks().forEach((element) => {
+    element.stop();
+  });
 
-
-
-
-// factory function
-
-// function factoryFunction(name,age){
-//     return {
-//         name : name,
-//         age : age,
-
-//         greet: function(){
-//             console.log(`Hi ${this.name}`);
-//         },
-
-//         getAge : () => {
-//             console.log(`You are ${this.age} years old`);
-//         }
-//     }
-// }
-
-// let obj = new constructorFunction("geetansh",22);
-// obj.greet();
-// obj.getAge();
-
-
-
-
-
-
-// constructor function 
-// function constructorFunction(name,age){
-//     this.name = name,
-//     this.age = age,
-
-//     this.greet = function(){
-//         console.log(`Hi ${this.name}`);
-//     },
-
-//     this.getAge = () => {
-//         console.log(`You are ${this.age} years old`);
-//     }
-// }
-
-// constructorFunction.prototype.hello = () => {
-//     console.log("Hello");
-// }
-
-// let obj = new constructorFunction("geetansh",22);
-// let obj1 = new constructorFunction("geetansh",22);
-// let obj2 = new constructorFunction("geetansh",22);
-// obj.greet();
-// obj.getAge();
-
-
-
-
-
-
-// object literal
-
-// let obj = {
-//     name:"Geetansh",
-//     greet : function() { 
-//         console.log(`Hello ${this.name}`);
-//     }
-// }
-
-// obj.greet()
-
-
-
-
-
-
-// class based
-
-// let pro = Promise. any([
-//     Promise.reject("A"),
-//     Promise.reject("B"),
-//     Promise.reject("C"),
-//     Promise.resolve("D"),
-// ])
-
-// pro.then((result) => {
-//     console.log(result);
-// }).catch((err) => {
-//     console.log(err);
-// });
-
-// let obj = {}
-
-// function abc(){}
-
-// abc.ca
-
-
-
-
+  stream = null;
+  document.querySelector("video").srcObject = null;
+}
