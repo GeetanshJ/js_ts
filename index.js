17ba13284688cc8614d1c6ea8d153669e4908a52
Geetansh Jain <jaingeetansh@gmail.com>
2025-06-20 15:40:18 +0530

    .

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
-document.addEventListener("DOMContentLoaded",(e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
 
   let stream;
-let face = "environment";
+  let face = "environment";
 
-async function play() {
-  stream = await navigator.mediaDevices.getUserMedia({
-    video: { facingMode: face },
-    audio: true,
-  });
-  document.querySelector("video").srcObject = stream;
-}
-
-async function pauseAudio() {
-  let audio = stream.getAudioTracks();
-  if(audio.length){
-    audio[0].enabled = false;
+  async function play() {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: face },
+      audio: true,
+    });
+    document.querySelector("video").srcObject = stream;
   }
-}
 
-async function resumeAudio() {
-  let audio = stream.getAudioTracks();
-  if(audio.length){
-    audio[0].enabled = true;
+  async function pauseAudio() {
+    let audio = stream.getAudioTracks();
+    if (audio.length) {
+      audio[0].enabled = false;
+    }
   }
-}
 
-async function pauseCamera() {
-  let cam = stream.getVideoTracks();
-  if(cam.length){
-    cam[0].enabled = false;
+  async function resumeAudio() {
+    let audio = stream.getAudioTracks();
+    if (audio.length) {
+      audio[0].enabled = true;
+    }
   }
-}
 
-async function resumeCamera() {
-  let cam = stream.getVideoTracks();
-  if(cam.length){
-    cam[0].enabled = true;
+  async function pauseCamera() {
+    let cam = stream.getVideoTracks();
+    if (cam.length) {
+      cam[0].enabled = false;
+    }
   }
-}
 
-async function switchCamera() {
-    face = face === "user" ? "environment" : "user"
-    await play()
-}
+  async function resumeCamera() {
+    let cam = stream.getVideoTracks();
+    if (cam.length) {
+      cam[0].enabled = true;
+    }
+  }
 
-})
+  async function switchCamera() {
+    face = face === "user" ? "environment" : "user";
+    await play();
+  }
+
+  play()
+});
